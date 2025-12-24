d6d22ecf6b134e29cd70dc59f375bba52c64966c
songtov <songchiho777@gmail.com>
2025-12-24 23:29:26 +0900

    make non-duplicaed messages

--- app/workflow/state.py
+++ app/workflow/state.py
@@ -1,5 +1,5 @@
 from typing import TypedDict, List, Dict, Annotated
-import operator
+
 
 class AgentType:
     MASTER = "MASTER_AGENT"
@@ -10,7 +10,13 @@ class AgentType:
 def last_write_wins(old: str, new: str) -> str:
     return new
 
+
+def replace_messages(old: List[Dict], new: List[Dict]) -> List[Dict]:
+    """Replace messages list instead of concatenating to avoid duplicates"""
+    return new if new else old
+
+
 class CustomState(TypedDict):
-    messages: Annotated[List[Dict], operator.add]
+    messages: Annotated[List[Dict], replace_messages]
     prev_node: Annotated[str, last_write_wins]
     next_step: Annotated[str, last_write_wins]
